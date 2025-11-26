2ceec7b0711125dd33d52c3a7dd619e921a87ba0
rzht97 <105182747+rzht97@users.noreply.github.com>
2025-11-26 19:53:02 +0700

    Add files via upload

---
+++ application/views/dev/pengumuman/lhkpn.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <title>Laporan Harta Kekayaan Pejabat Negara - PPID Kab. Sumedang</title>
+    <?php $this->load->view('dev/partials/head.php') ?>
+    <link href="<?= base_url() ?>inverse/plugins/bower_components/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    
+
+    <div class="preloader">
+        <div class="preloader__image"></div>
+    </div>
+    <!-- /.preloader -->
+    <div class="page-wrapper">
+        <?php $this->load->view('dev/partials/header.php') ?>
+
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content"></div>
+            <!-- /.sticky-header__content -->
+        </div>
+        <!-- /.stricky-header -->
+
+        <!--Page Header Start-->
+        <section class="page-header">
+            <div class="page-header__bg"></div>
+            <!-- /.page-header__bg -->
+            <div class="page-header-shape-1"></div>
+            <!-- /.page-header-shape-1 -->
+            <div class="page-header-shape-2"></div>
+            <!-- /.page-header-shape-2 -->
+            <div class="page-header-shape-3"></div>
+            <!-- /.page-header-shape-3 -->
+            <div class="container">
+                <div class="page-header__inner">
+                    <ul class="thm-breadcrumb list-unstyled">
+                        <li><a href="https://ppid.sumedangkab.go.id">Beranda</a></li>
+                        <li><span>/</span></li>
+                        <li>LHKPN</li>
+                    </ul>
+                    <h2>LAPORAN HARTA KEKAYAAN PEJABAT NEGARA</h2>
+                </div>
+            </div>
+        </section>
+        <!--Page Header End-->
+        <section class="faqs-page">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="accrodion-grp" data-grp-name="faq-one-accrodion">
+                            <div class="accrodion">
+                                <div class="accrodion-title">
+                                    <h4><span>.</span> Bupati Sumedang Dony Ahmad Munir</h4>
+                                </div>
+                                <div class="accrodion-content">
+                                    <div class="inner">
+                                        <a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKPN_Bupati Sumedang Dony Achmad Munir.pdf" ><p>Tahun 2021</p></a>
+										<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKPN BUPATI 2022.pdf" ><p>Tahun 2022</p></a>
+										<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKPN Bupati 2024.pdf" ><p>Tahun 2024</p></a>
+                                    </div><!-- /.inner -->
+                                </div>
+                            </div>
+							<div class="accrodion">
+                                <div class="accrodion-title">
+                                    <h4><span>.</span>Wakil Bupati Sumedang Fajar Aldila</h4>
+                                </div>
+                                <div class="accrodion-content">
+                                    <div class="inner">
+                                        <a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKPN Wakil Bupati 2024.pdf" ><p>Tahun 2024</p></a>
+										
+                                    </div><!-- /.inner -->
+                                </div>
+                            </div>
+							<div class="accrodion">
+                                <div class="accrodion-title">
+                                    <h4><span>.</span> Sekretaris Daerah Kab. Sumedang Tuti Ruswati</h4>
+                                </div>
+                                <div class="accrodion-content">
+                                    <div class="inner">
+                                        <a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKPN Sekda 2023.pdf" ><p>Tahun 2023</p></a>
+										<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKPN Sekda 2024.pdf" ><p>Tahun 2024</p></a>
+										<!--<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/lhkpnsekda2022.pdf" ><p>Tahun 2022</p></a>-->
+                                    </div><!-- /.inner -->
+                                </div>
+                            </div>
+							<div class="accrodion">
+                                <div class="accrodion-title">
+                                    <h4><span>.</span> Kepala Dinas Komunikasi & Informatika, Persandian dan Statistik Kab. Sumedang</h4>
+                                </div>
+                                <div class="accrodion-content">
+                                    <div class="inner">
+                                        <a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKPN Kadiskominfo 2024.pdf" ><p>Tahun 2024</p></a>
+                                    </div><!-- /.inner -->
+                                </div>
+                            </div>
+							<div class="accrodion">
+                                <div class="accrodion-title">
+                                    <h4><span>.</span> LHKPN Pejabat Eselon II pada Lingkup Pemerintah Kabupaten Sumedang</h4>
+                                </div>
+                                <div class="accrodion-content">
+                                    <div class="inner">
+                                        <a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/lhkpneselon22022.pdf" ><p>Tahun 2022</p></a>
+										<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/Pejabat Eselon II.pdf" ><p>Tahun 2023</p></a>
+										<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/lhkpneselon22024.pdf" ><p>Tahun 2024</p></a>
+                                    </div><!-- /.inner -->
+                                </div>
+                            </div>
+							<div class="accrodion">
+                                <div class="accrodion-title">
+                                    <h4><span>.</span>  Rekapitulasi jumlah Wajib LHKSN dalam lingkup Pemerintah Daerah Kabupaten Sumedang </h4>
+                                </div>
+                                <div class="accrodion-content">
+                                    <div class="inner">
+                                        <a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/lhksn2022.pdf" ><p>Tahun 2022</p></a>
+										<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKAN 2023.pdf" ><p>Tahun 2023</p></a>
+										<a target = "_blank" href = "https://ppid.sumedangkab.go.id/upload/lhkpn/LHKAN 2024.pdf" ><p>Tahun 2024</p></a>
+                                    </div><!-- /.inner -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php $this->load->view("dev/partials/sectionapp.php") ?>
+
+        <!--Site Footer One Start-->
+        <?php $this->load->view("dev/partials/footer.php") ?>
+        <!--Site Footer One End-->
+
+
+    </div>
+    <!-- /.page-wrapper -->
+
+
+    <?php $this->load->view("dev/partials/mobilemenu.php") ?>
+    <!-- /.mobile-nav__wrapper -->
+
+    <div class="search-popup">
+        <div class="search-popup__overlay search-toggler"></div>
+        <!-- /.search-popup__overlay -->
+        <div class="search-popup__content">
+            <form action="#">
+                <label for="search" class="sr-only">search here</label>
+                <!-- /.sr-only -->
+                <input type="text" id="search" placeholder="Search Here..." />
+                <button type="submit" aria-label="search submit" class="thm-btn">
+                    <i class="icon-magnifying-glass"></i>
+                </button>
+            </form>
+        </div>
+        <!-- /.search-popup__content -->
+    </div>
+    <!-- /.search-popup -->
+
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+
+
+    <?php $this->load->view('dev/partials/js.php') ?>
+
+    <script src="<?= base_url() ?>inverse/plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?= base_url() ?>inverse/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="<?= base_url() ?>inverse/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+    <!--slimscroll JavaScript -->
+    <script src="<?= base_url() ?>inverse/js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="<?= base_url() ?>inverse/js/waves.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="<?= base_url() ?>inverse/js/custom.min.js"></script>
+    <script src="<?= base_url() ?>inverse/plugins/bower_components/datatables/jquery.dataTables.min.js"></script>
+    <!-- start - This is for export functionality only -->
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+    <!-- end - This is for export functionality only -->
+    
+    <!--Style Switcher -->
+    <script src="<?= base_url() ?>inverse/plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+</body>
+
+
+</html>
